b01095e8dc512f0f304d77f79dad97f9e85e28be
Link4Electronics <link4electronics@gmail.com>
2026-03-08 22:34:21 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	gtk3	       \
+	libdecor  	   \
+	sdl2	 	   \
+	vulkan-headers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -16,11 +20,18 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use X64 and ARM64 for the zip links
+	x86_64)  zip_arch=Linux-X64-Release;;
+	aarch64) zip_arch=Linux-ARM64-Release;;
+esac
+ZIP_LINK=$(wget -qO- https://api.github.com/repos/sonicdcer/Starfox64Recomp/releases \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*Starfox64Recompiled.*$zip_arch.zip")
+echo "$ZIP_LINK" | awk -F'/' '{gsub(/^v/, "", $(NF-1)); print $(NF-1); exit}' > ~/version
+wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+bsdtar -xvf /tmp/app.zip -C .
+bsdtar -xvf ./Starfox64Recompiled.tar.gz -C ./AppDir/bin
+wget -q -O ./AppDir/bin/recompcontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
