f3a9f8e52105279e2cdac63f09093d7e2a12960c
Link4Electronics <link4electronics@gmail.com>
2026-03-08 22:34:21 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,25 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm sdl2-compat	vulkan-headers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use X64 and ARM64 for the zip links
+	x86_64)  zip_arch=Linux-X64-Release;;
+	aarch64) zip_arch=Linux-ARM64-Release;;
+esac
+TAG=$(wget -qO- "https://gitlab.com/api/v4/projects/sonicdcer%2FStarfox64Recomp/releases?per_page=1" \
+      | sed 's/[()",{} ]/\n/g' | grep -A2 '^tag_name$' | tail -n1)
+ZIP_LINK="https://gitlab.com/api/v4/projects/sonicdcer%2FStarfox64Recomp/packages/generic/starfox64recompiled$(echo "${zip_arch%-Release}" | tr -d '-' | tr '[:upper:]' '[:lower:]')/${TAG#v}/Starfox64Recompiled-${TAG}-${zip_arch}.zip"
+echo "${TAG#v}" > ~/version
+wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+bsdtar -xvf /tmp/app.zip -C .
+bsdtar -xvf ./Starfox64Recompiled.tar.gz -C ./AppDir/bin
+wget -q -O ./AppDir/bin/recompcontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
