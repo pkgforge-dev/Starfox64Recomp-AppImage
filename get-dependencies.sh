002d9720eeed07baa81124d0bcc08fc901511ddb
Link4Electronics <link4electronics@gmail.com>
2026-03-29 12:55:05 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,6 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	gtk3	       \
 	libdecor  	   \
 	sdl2	 	   \
 	vulkan-headers
