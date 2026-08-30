6ec531ed8d61982b0551cbc7b9071fa36c0dfbc6
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:25:16 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://gitlab.com/sonicdcer/Starfox64Recomp/-/raw/main/icons/512.png?ref_type=heads
 export DESKTOP=https://gitlab.com/sonicdcer/Starfox64Recomp/-/raw/main/.github/linux/Starfox64Recompiled.desktop?ref_type=heads
 export STARTUPWMCLASS=Starfox64Recompiled
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
